94c3af69e25357e6f740afa850223bb8f5f73803
Saad Shekhani <saadoshekhani@gmail.com>
2024-05-10 18:03:43 +0200

    Collect count from DB with new count

--- src/models/findUpdate.js
+++ src/models/findUpdate.js
@@ -2,32 +2,56 @@
 const Words = require('./wordsCollection') //words Collection.
 
 // ------------ FUNCTIONS ----------------------
-const hello = async (count) => {
-    const filter = { _id: '663b7179159ae8b14b7c7ce3' }; // Hello ID
+const findAndUpdateWord = async (id, count) => {
+    const filter = { _id: id };
     const update = { count: count };
     const options = { upsert: true, new: true, setDefaultsOnInsert: true };
-    const doc = await Words.findOneAndUpdate(filter, update, options);
-    console.log(`Hello ID`);
-    count = await doc.save()
-    return count;
+    return await Words.findOneAndUpdate(filter, update, options);
+};
+const getIdCount = async (id) => {
+    const word = await Words.findOne({ _id: id });
+    return word ? word.count : null;
+};
+
+// ------------ EXPORTS ----------------------
+const hello = async (count) => {
+    const id = '663b7179159ae8b14b7c7ce3';
+    const currentCount = await getIdCount(id);
+    if (currentCount === null) {
+        console.log('No document found for hello');
+        return null;
+    }
+    console.log('Current count:', currentCount); //Current count
+    const updatedWord = await findAndUpdateWord(id, count + currentCount);
+    console.log(`Word with ID ${id} updated successfully.`);
+    await updatedWord.save();
+    return updatedWord.count
 }
 const hi = async (count) => {
-    const filter = { _id: '663b86f380ab9af0e9bbf47c' }; // Hi ID
-    const update = { count: count };
-    const options = { upsert: true, new: true, setDefaultsOnInsert: true };
-    const doc = await Words.findOneAndUpdate(filter, update, options);
-    console.log(`Hi ID`);
-    count = await doc.save()
-    return count;
+    const id = '663b86f380ab9af0e9bbf47c';
+    const currentCount = await getIdCount(id);
+    if (currentCount === null) {
+        console.log('No document found for hello');
+        return null;
+    }
+    console.log('Current count:', currentCount); //Current count
+    const updatedWord = await findAndUpdateWord(id, count + currentCount);
+    console.log(`Word with ID ${id} updated successfully.`);
+    await updatedWord.save();
+    return updatedWord.count
 }
 const lol = async (count) => {
-    const filter = { _id: '663b86a7f955b5602e3132f3' }; // LOL ID
-    const update = { count: count };
-    const options = { upsert: true, new: true, setDefaultsOnInsert: true };
-    const doc = await Words.findOneAndUpdate(filter, update, options);
-    console.log(`LOL ID`);
-    count = await doc.save()
-    return count;
+    const id = '663b86a7f955b5602e3132f3';
+    const currentCount = await getIdCount(id);
+    if (currentCount === null) {
+        console.log('No document found for hello');
+        return null;
+    }
+    console.log('Current count:', currentCount); //Current count
+    const updatedWord = await findAndUpdateWord(id, count + currentCount);
+    console.log(`Word with ID ${id} updated successfully.`);
+    await updatedWord.save();
+    return updatedWord.count
 }
 module.exports = {
     hello: hello,
